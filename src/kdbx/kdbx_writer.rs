ee8b53f827797e806548ce92856ebc89c3ef4246
rustam <rst@avangardo.com>
2021-11-18 18:28:31 +0300

    Write Hash and HMAC into header

--- src/kdbx/kdbx_writer.rs
+++ src/kdbx/kdbx_writer.rs
@@ -1,11 +1,13 @@
 use std::io::Write;
 
 use byteorder::{ByteOrder, LittleEndian};
+use hmac_sha256::{HMAC, Hash};
 use rand::Rng;
 
 use super::consts::*;
 use super::crypt::ciphers::Cipher;
 use super::decompress::Decompress;
+use super::key::{CompositeKey, Key, PasswordKey};
 use super::result::Result;
 use super::variant_dictionary::{VariantDictionary, VariantDictionaryValue};
 
@@ -25,11 +27,19 @@ impl KdbxWriter {
         let mut buf = [0u8; 1024];
         let mut idx: usize = 0;
 
+        // Write header's signature and versions
         KdbxWriter::write_base_signature(&mut buf, &mut idx);
         KdbxWriter::write_version_signature(&mut buf, &mut idx);
         KdbxWriter::write_file_version(&mut buf, &mut idx);
 
-        // KDF header
+        // Write Master Seed
+        KdbxWriter::write_header_field_prefix(&mut buf, &mut idx, HeaderFieldId::MasterSeed, 32);
+        let mut master_seed = [0u8; 32];
+        rand::thread_rng().fill(&mut master_seed);
+        buf[idx..idx + 32].copy_from_slice(&master_seed);
+        idx += 32;
+
+        // Write KDF header
         KdbxWriter::write_header_field_prefix(&mut buf, &mut idx, HeaderFieldId::KdfParameters, 0); // will update 0 to actual value below
         let pre_kdf_idx = idx;
 
@@ -50,11 +60,20 @@ impl KdbxWriter {
             (idx - pre_kdf_idx) as u32,
         );
 
-        // End of header
+        // End of header [0xD 0xA 0xD 0xA]
         KdbxWriter::write_header_field_prefix(&mut buf, &mut idx, HeaderFieldId::EndOfHeader, 4);
         buf[idx..idx + 4].copy_from_slice(&[0xd, 0xa, 0xd, 0xa]);
         idx += 4;
 
+        // Write Hash and HMAC of header
+        let key = PasswordKey::new(password);
+        let mut composite_key = CompositeKey::new(master_seed.to_vec(), seed.to_vec(), ROUNDS);
+        composite_key.add(key);
+        composite_key.transform();
+
+        KdbxWriter::write_SHA_HMAC(&mut buf, &mut idx, &composite_key);
+
+        // Flush buffer to output stream
         stream.write_all(&mut buf)?;
 
         Ok(())
@@ -73,6 +92,16 @@ impl KdbxWriter {
     fn write_file_version(buf: &mut [u8], idx: &mut usize) {
         LittleEndian::write_u32(&mut buf[*idx..*idx + 4], FILE_FORMAT_4);
         *idx += 4;
+    }
+
+    fn write_SHA_HMAC(buf: &mut [u8], idx: &mut usize, key: &impl Key) {
+        let sha256 = Hash::hash(&buf[..*idx]);
+        let hmac_sha256 = HMAC::mac(&buf[..*idx], &key.header_hmac_key());
+
+        buf[*idx..*idx + 32].copy_from_slice(&sha256);
+        *idx += 32;
+        buf[*idx..*idx + 32].copy_from_slice(&hmac_sha256);
+        *idx += 32;
     }
 
     fn write_header_field_prefix(
